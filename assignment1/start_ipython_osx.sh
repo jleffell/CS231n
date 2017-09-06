45dc02ec61bc9f5bed7c3524891db1ae8bdbc3cc
Joshua Leffell <joshua.leffell@sri.com>
2017-09-06 00:33:02 -0700

    Modified startup script to call my frameworkpython

--- assignment1/start_ipython_osx.sh
+++ assignment1/start_ipython_osx.sh
@@ -1,4 +1,4 @@
 # Assume the virtualenv is called .env
-
-cp -n frameworkpython ${VIRTUAL_ENV}/bin
-${VIRTUAL_ENV}/bin/frameworkpython -m IPython notebook
+#cp -n frameworkpython ${VIRTUAL_ENV}/bin
+#${VIRTUAL_ENV}/bin/frameworkpython -m IPython notebook
+frameworkpython -m IPython notebook
